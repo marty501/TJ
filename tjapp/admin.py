8c5e085105f2bdc2ec834af8c86870c2cc0b2127
Martin Raychev <martyraychev@gmail.com>
2018-01-15 21:46:14 +0000

    All other files

---
+++ tjapp/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from .models import Post
+from .models import Company
+from .models import Journey
+from .models import Station
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'WebUrl', 'WebUrlContactUs', 'CustomerServicePhone')
+    search_fields = ('Name', 'WebUrl')
+
+
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Created', 'Updated')
+    search_fields = ('Name',)
+
+    ordering = ['Name', 'Created', 'Updated']
+
+class JourneyAdmin(admin.ModelAdmin):
+    list_display = ('DepartureStation', 'DelayAtDepartureStationMins')
+    search_fields = ('DepartureStation', 'ArrivalStation', 'DelayAtDepartureStationMins')
+    date_hierarchy = 'Created'
+    ordering = ['Company', 'ScheduledDepartureTime', 'Created', 'Updated']
+
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Station, StationAdmin)
+admin.site.register(Journey, JourneyAdmin)
+
+admin.site.register(Post, PostAdmin)
---
+++ tjapp/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+
+class Company(models.Model):
+    Name = models.CharField(max_length=50)
+    WebUrl = models.URLField(max_length=250, null=True)
+    WebUrlContactUs = models.URLField(max_length=250, blank=True, default='')
+    CustomerServicePhone = models.CharField(max_length=25, blank=True, default='')
+
+    Created = models.DateTimeField(auto_now_add=True)
+    Updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s' % (self.Name)
+
+
+class Station(models.Model):
+    DEFAULT_PK = 1
+    Name = models.CharField(max_length=100)
+
+    Created = models.DateTimeField(auto_now_add=True)
+    Updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s' % (self.Name)
+
+
+class Journey(models.Model):
+    Company = models.ForeignKey(Company, related_name='company_journeys')
+    User = models.ForeignKey(User, related_name='user_journey')
+    DepartureStation = models.ForeignKey(Station, related_name='journey_station_dep', default=Station.DEFAULT_PK)
+    ArrivalStation = models.ForeignKey(Station, related_name='journey_station_arr', default=Station.DEFAULT_PK)
+    DelayAtDepartureStationMins = models.PositiveSmallIntegerField()
+    IsCancelled = models.BooleanField()
+    ScheduledDepartureTime = models.TimeField();
+    ActualArrivalTime = models.TimeField(blank=True, null=True)
+    Notes = models.CharField(max_length=250, blank=True, default='')
+    IsVerified = models.BooleanField()
+
+    Created = models.DateTimeField(auto_now_add=True)
+    Updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Dep:%s Arr:%s DELAY: %d' % (
+            self.DepartureStation.Name, self.ArrivalStation.Name, self.DelayAtDepartureStationMins)
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('tjapp:post_detail', args=[self.publish.year,
+                                                  self.publish.strftime('%m'),
+                                                  self.publish.strftime('%d'),
+                                                  self.slug])
+
+
+class Meta:
+    ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+# Create your models here.
