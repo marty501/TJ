f295b2a4b33b206cc450a52c6fd0b813f62a8640
Martin Raychev <martyraychev@gmail.com>
2018-02-21 22:21:26 +0000

    Added more views

--- tjapp/templates/tjapp/account/urls.py
+++ tjapp/templates/tjapp/account/urls.py
@@ -1,18 +1,27 @@
+import django
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout, logout_then_login
 from . import views
 
 urlpatterns = [
-
     # url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', views.dashboard, name='dashboard'),
+
+    url(r'^register/$', views.register, name='register'),
+    url(r'^edit/$', views.edit, name='edit'),
 
     # login / logout urls
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
 
-    # Register
-    url(r'^register/$', views.register, name='register'),
+    # change password urls
+    url(r'^password-change/$', django.contrib.auth.views.password_change, name='password_change'),
+    url(r'^password-change/done/$', django.contrib.auth.views.password_change_done, name='password_change_done'),
 
-    url(r'^$', views.dashboard, name='dashboard'),
+    # restore password urls
+    url(r'^password-reset/$', django.contrib.auth.views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', django.contrib.auth.views.password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', django.contrib.auth.views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', django.contrib.auth.views.password_reset_complete, name='password_reset_complete'),
 ]
--- tjapp/templates/tjapp/account/views.py
+++ tjapp/templates/tjapp/account/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm, UserEditForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import LoginForm, UserRegistrationForm, UserEditForm
 
 
 def user_login(request):
@@ -62,3 +63,28 @@ def edit(request):
         user_form = UserEditForm(instance=request.user)
         # profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request, 'tjapp/account/edit.html', {'user_form': user_form, })
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        #profile_form = ProfileEditForm(instance=request.user.profile,data=request.POST,files=request.FILES)
+        if user_form.is_valid(): # and profile_form.is_valid():
+            user_form.save()
+            #profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        #profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'tjapp/account/edit.html', {'user_form': user_form,
+                                                 # 'profile_form': profile_form
+                  }
+                  )
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'tjapp/account/dashboard.html', {'section': 'dashboard'})
